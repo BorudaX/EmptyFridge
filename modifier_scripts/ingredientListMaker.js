8eaa929fdc4b09d4bfcf4dc1bd579f1fdd67b98d
Alan Liang <alanliang@live.com>
2023-03-05 17:21:25 -0600

    Hi

--- modifier_scripts/ingredientListMaker.js
+++ modifier_scripts/ingredientListMaker.js
@@ -10,31 +10,33 @@ var obj = {};
 var count = 0;
 
 filenames.forEach(function (filename) {
+    if (filename.includes("RecipeNLG")) {
+        console.log("Processing " + filename);
+        csv = JSON.parse(fs.readFileSync(dirname + filename, 'utf-8'));
 
-    console.log("Processing " + filename);
-    csv = JSON.parse(fs.readFileSync(dirname + filename, 'utf-8'));
+        Object.keys(csv).forEach(function (row) {
 
-    Object.keys(csv).forEach(function (row) {
-
-        // Iterate through ingredient
-        var NER = csv[row].NER;
-        if (NER !== undefined) {
-            nerLength = NER.length;
-        }
-        for (var i = 0; i < nerLength; i++) {
-            if (obj[NER[i]]) {
-                obj[NER[i]].push(row); // If ingredient is a title in obj, add recipe to obj's part
+            // Iterate through ingredient
+            var NER = csv[row].NER;
+            if (NER !== undefined) {
+                nerLength = NER.length;
+            }
+        
+            for (let i = 0; i < NER.length; i++) {
+                if (obj[NER[i]] == undefined) {
+                    obj[NER[i]] = {};
+                }
             
+                // if (obj[NER[i]][row] == undefined) {
+                obj[NER[i]][row] = 1 / nerLength;
+                // }
             }
-            else {
-                obj[NER[i]] = [row]; // If ingredient is not a title in obj, add ingredient as title and recipe as value
-            }
-        }
        
-    });
+        });
 
 
-    console.log("Done " + filename);
+        console.log("Done " + filename);
+    }
 });
 
-fs.writeFileSync(dirname + "Ingredient" + count + ".json", JSON.stringify(obj));
+fs.writeFileSync(dirname + "IngredientDict" + count + ".json", JSON.stringify(obj));

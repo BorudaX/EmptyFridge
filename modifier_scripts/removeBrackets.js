2a22dddad5dbf5a1a9d82ca3a16851c444149d41
Girish Dodda <girishbdodda@gmail.com>
2023-03-05 23:21:56 -0600

    thing

--- modifier_scripts/removeBrackets.js
+++ modifier_scripts/removeBrackets.js
@@ -1,25 +1,31 @@
 var fs = require('fs');
 
-var dirname = "E:/csv/datajson/";
+var dirname = "C:/Users/giris/Repos/EmptyFridge/data/";
 var filenames = fs.readdirSync(dirname);
 
 console.log(filenames);
 
 filenames.forEach(function (filename) {
-    console.log("Processing " + filename);
-    csv = JSON.parse(fs.readFileSync(dirname + filename, 'utf-8'));
+    if (filename.includes("RecipeNLG")) {
+        console.log("Processing " + filename);
+        csv = JSON.parse(fs.readFileSync(dirname + filename, 'utf-8'));
+        keys = Object.keys(csv);
 
-    csv.forEach(function (row) {
-        row.NER[0] = row.NER[0].replace("[", "");
-        row.NER[row.NER.length - 1] = row.NER[row.NER.length - 1].replace("]", "");
+        keys.forEach(function (row) {
+            // row.NER[0] = row.NER[0].replace("[", "");
+            // row.NER[row.NER.length - 1] = row.NER[row.NER.length - 1].replace("]", "");
 
-        // row.directions[0] = row.directions[0].replace("[", "");
-        // row.directions[row.directions.length - 1] = row.directions[row.directions.length - 1].replace("]", "");
+            // row.directions[0] = row.directions[0].replace("[", "");
+            // row.directions[row.directions.length - 1] = row.directions[row.directions.length - 1].replace("]", "");
 
-        // delete row.source;
-        // delete row.i;
-    });
+            csv[row].ingredients[0] = csv[row].ingredients[0].replace("[", "");
+            csv[row].ingredients[csv[row].ingredients.length - 1] = csv[row].ingredients[csv[row].ingredients.length - 1].replace("]", "");
 
-    fs.writeFileSync(dirname + filename, JSON.stringify(csv));
-    console.log("Done " + filename);
+            // delete row.source;
+            // delete row.i;
+        });
+
+        fs.writeFileSync(dirname + filename, JSON.stringify(csv));
+        console.log("Done " + filename);
+    }
 });
